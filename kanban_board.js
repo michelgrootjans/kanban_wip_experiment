3a862504710c744b28242146ca0b69fa65c781db
Michel Grootjans <michel.grootjans@gmail.com>
2015-08-16 20:43:58 +0200

    introduced self

--- kanban_board.js
+++ kanban_board.js
@@ -1,43 +1,45 @@
 function kanban_board() {
-  this.todo = ko.observable(100);
-  this.busy = ko.observable(0);
-  this.done = ko.observable(0);
-  this.number_of_iterations = ko.observable(0);
+  var self = this;
+  
+  self.todo = ko.observable(100);
+  self.busy = ko.observable(0);
+  self.done = ko.observable(0);
+  self.number_of_iterations = ko.observable(0);
 
-  this.iterate = function(){
-    if(this.busy() > 0){
-      var work_finished = Math.min(this.busy(), this.roll());
-      this.busy(this.busy() - work_finished);
-      this.done(this.done() + work_finished);
+  self.iterate = function(){
+    if(self.busy() > 0){
+      var work_finished = Math.min(self.busy(), self.roll());
+      self.busy(self.busy() - work_finished);
+      self.done(self.done() + work_finished);
     }
 
-    if(this.todo() > 0){
-      var work_started = Math.min(this.todo(), this.roll());
-      this.todo(this.todo() - work_started);
-      this.busy(this.busy() + work_started);
+    if(self.todo() > 0){
+      var work_started = Math.min(self.todo(), self.roll());
+      self.todo(self.todo() - work_started);
+      self.busy(self.busy() + work_started);
     }
 
-    this.number_of_iterations(this.number_of_iterations() + 1);
+    self.number_of_iterations(self.number_of_iterations() + 1);
   };
 
-  this.round = function(number){
+  self.round = function(number){
     return Math.round(number * 100)/100
   };
 
-  this.roll = function(){
+  self.roll = function(){
     return Math.floor((Math.random() * 6) + 1);
   };
 
-  this.reset = function(){
-    this.todo(100);
-    this.busy(0);
-    this.done(0);
-    this.number_of_iterations(0);
+  self.reset = function(){
+    self.todo(100);
+    self.busy(0);
+    self.done(0);
+    self.number_of_iterations(0);
   };
 
-  this.througput = ko.computed(function(){
-    return this.round(this.done() / this.number_of_iterations());
-  }, this);
+  self.througput = ko.computed(function(){
+    return self.round(self.done() / self.number_of_iterations());
+  }, self);
 };
 
 ko.applyBindings(kanban_board);
