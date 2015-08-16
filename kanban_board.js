3478b62ede39850bcabfe7865ac8517f128c8573
Michel Grootjans <michel.grootjans@gmail.com>
2015-08-16 20:12:43 +0200

    simplest working kanban board

--- kanban_board.js
+++ kanban_board.js
@@ -1,7 +1,23 @@
 var kanban_board = {
   backlog: ko.observable(100),
   busy: ko.observable(0),
-  done: ko.observable(0)
+  done: ko.observable(0),
+
+  iterate: function(){
+    if(this.busy() > 0){
+      var work_finished = Math.min(this.busy(), this.roll());
+      this.busy(this.busy() - work_finished);
+      this.done(this.done() + work_finished);
+    }
+
+    var work_started = Math.min(this.backlog(), this.roll());
+    this.backlog(this.backlog() - work_started);
+    this.busy(this.busy() + work_started);
+  },
+
+  roll: function(){
+    return Math.floor((Math.random() * 6) + 1);
+  }
 };
 
-ko.applyBindings(kanban_board)b
+ko.applyBindings(kanban_board);
