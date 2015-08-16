16c7b3bdd019bba5b2b403e6899109e6c024edfe
Michel Grootjans <michel.grootjans@gmail.com>
2015-08-16 21:53:09 +0200

    moved from ints to arrays

--- kanban_board.js
+++ kanban_board.js
@@ -10,31 +10,40 @@ function roll(sides){
 function kanban_board() {
   var self = this;
   
-  self.todo = ko.observable();
-  self.busy = ko.observable();
-  self.done = ko.observable();
+  self.todo = ko.observableArray();
+  self.busy = ko.observableArray();
+  self.done = ko.observableArray();
   self.number_of_iterations = ko.observable();
   self.maximum_transfer = ko.observable(6);
 
   self.reset = function(){
-    self.todo(200);
-    self.busy(0);
-    self.done(0);
+    self.todo.removeAll();
+    for(i=0; i < 200; i++){
+      self.todo.push(1);
+    }
+    self.busy.removeAll();
+    self.done.removeAll();
     self.number_of_iterations(0);
   };
   self.reset();
 
   self.iterate = function(){
-    if(self.busy() > 0){
-      var work_finished = Math.min(self.busy(), roll(self.maximum_transfer()));
-      self.busy(self.busy() - work_finished);
-      self.done(self.done() + work_finished);
+    if(self.busy().length > 0){
+      var work_finished = Math.min(self.busy().length, roll(self.maximum_transfer()));
+      // self.busy(self.busy() - work_finished);
+      // self.done(self.done() + work_finished);
+      for(i=0; i < work_finished; i++){
+        self.done.push(self.busy.pop());
+      }
     }
 
-    if(self.todo() > 0){
-      var work_started = Math.min(self.todo(), roll(self.maximum_transfer()));
-      self.todo(self.todo() - work_started);
-      self.busy(self.busy() + work_started);
+    if(self.todo().length > 0){
+      var work_started = Math.min(self.todo().length, roll(self.maximum_transfer()));
+      // self.todo(self.todo() - work_started);
+      // self.busy(self.busy() + work_started);
+      for(i=0; i < work_started; i++){
+        self.busy.push(self.todo.pop());
+      }
     }
 
     self.number_of_iterations(self.number_of_iterations() + 1);
@@ -46,11 +55,11 @@ function kanban_board() {
   };
 
   self.is_busy = ko.computed(function(){
-    return self.todo() + self.busy() > 0;
+    return self.todo().length + self.busy().length > 0;
   });
 
   self.througput = ko.computed(function(){
-    return round(self.done() / self.number_of_iterations());
+    return round(self.done().length / self.number_of_iterations());
   }, self);
 };
 
